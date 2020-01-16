5cb240ddf317df58182ff95bb45fd6bf5affee39
bmg23 <bmg23@maroon32.cs.calvin.edu>
2020-01-16 11:46:06 -0500

    Better user input, initial css

--- main/mainPage.js
+++ main/mainPage.js
@@ -1,19 +1,16 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-   }
+//Event List
+events = ["400 Free-R Mixed", "400 Medley-R Mixed", "50 Free", "100 Free", "200 Free",
+          "400 Free", "800 Free", "1500 Free","50 Back", "100 Back", "200 Back", "50 Breast",
+          "100 Breast", "200 Breast", "50 Fly", "100 Fly", "200 Fly", "200 IM", "400 IM",
+          "400 Free Relay",  "800 Free Relay", "400 Medley Relay"]; 
+
+
+
+var eventSelect = document.getElementById('events');
+
+for (const event in events) {
+  var newEventOption = document.createElement("option");
+  newEventOption.text = `${events[event]}`;
+  eventSelect.add(newEventOption); 
 }
+
