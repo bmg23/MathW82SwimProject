282f67e07c672a98064233d93ce0f0d9652f10e8
bmg23 <bmg23@maroon32.cs.calvin.edu>
2020-01-20 11:43:00 -0500

    Cleaned up data

--- main/mainPage.js
+++ main/mainPage.js
@@ -1,3 +1,9 @@
+/* Fill in selection boxes with data 
+ *
+ *
+ * 
+ */
+
 //Event List
 events = ["400 Free-R Mixed", "400 Medley-R Mixed", "50 Free", "100 Free", "200 Free",
           "400 Free", "800 Free", "1500 Free","50 Back", "100 Back", "200 Back", "50 Breast",
@@ -14,23 +20,72 @@ for (const event in events) {
   eventSelect.add(newEventOption); 
 }
 
-/* STACKOVERFLOW CODE 
-var allText =[];
-var allTextLines = [];
-var Lines = [];
+//Dates
 
-var txtFile = new XMLHttpRequest();
-txtFile.open("GET", "../Data/AllWorldRecordsProgression.txt", true);
-txtFile.onreadystatechange = function()
-{
-    allText = txtFile.responseText;
-    allTextLines = allText.split(/\r\n|\n/);
-};
+dates = ["all"];
 
-require(['fs']);
-fs = require('fs'); 
-fs.readFile( "../Data/AllWorldRecordsProgression.txt", (err, data) => {
-    if(err) throw err;
-    console.log(data.toString()); 
-})
-*/ 
+
+/*
+Event format
+*/
+
+eventCSV = ["Men's 50 FR LCM", "Women's 50 FR LCM", "Men's 100 FR LCM", 
+            "Women's 100 FR LCM", "Men's 200 FR LCM", "Women's 200 FR LCM", 
+            "Men's 400 FR LCM", "Women's 400 FR LCM", "Men's 800 FR LCM", 
+            "Women's 800 FR LCM", "Men's 1500 FR LCM", "Women's 1500 FR LCM", 
+            "Men's 50 BK LCM", "Women's 50 BK LCM", "Men's 100 BK LCM", 
+            "Women's 100 BK LCM","Men's 200 BK LCM","Women's 200 BK LCM",
+            "Men's 50 BR LCM", "Women's 50 BR LCM", "Men's 100 BR LCM", 
+            "Women's 100 BR LCM", "Men's 200 BR LCM", "Women's 200 BR LCM",
+            "Men's 50 FL LCM", "Women's 50 FL LCM", "Men's 100 FL LCM", 
+            "Women's 100 FL LCM", "Men's 200 FL LCM", "Women's 200 FL LCM", 
+            "Men's 200 IM LCM", "Women's 200 IM LCM", "Men's 400 IM LCM", 
+            "Women's 400 IM LCM", "Men's 400 FR-R LCM", "Women's 400 FR-R LCM", 
+            "Mixed 400 FR-R LCM", "Men's 800 FR-R LCM", "Women's 800 FR-R LCM", 
+            "Men's 400 MED-R LCM", "Women's 400 MED-R LCM", "Mixed 400 MED-R LCM"]  
+
+/* Clean Up Data
+*/
+
+function includes(data, array) {
+  for (i of array) {
+    if(i == data) {
+      return true; 
+    }
+  }
+
+  return false; 
+}
+
+
+function cleanUpData(data) {
+  
+  var event = eventCSV[0]; 
+ 
+
+  for (line in data) {
+
+    //Get Event
+    const values = Object.values(data[line]); 
+    var value1 = values[0]; 
+    
+    //Check and Set Event
+    if(includes(value1, eventCSV)) {
+      event = value1; 
+    }
+
+    //Add Event
+    data[line].Event = event; 
+    
+
+    console.log(data[line]); 
+
+  }
+
+
+
+}
+
+
+d3.csv("AllWorldRecords.csv").then( cleanUpData ); 
+
