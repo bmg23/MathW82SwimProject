ecc214db6c99d233bdc694d08f53b6f63293b7c1
bmg23 <bmg23@maroon32.cs.calvin.edu>
2020-01-21 11:05:00 -0500

    More functions to clean data

--- main/mainPage.js
+++ main/mainPage.js
@@ -1,3 +1,15 @@
+/* Swimming Historical Time Progression
+ * 
+ * Authors: Brian Goins, Emily Gunderson
+ * Date: January 2020
+ * 
+ * Goal: Using the JS libary d3 we hope to create 
+ * an interactive graph of historical swimming records. 
+ */
+
+
+
+
 /* Fill in selection boxes with data 
  *
  *
@@ -21,8 +33,26 @@ for (const event in events) {
 }
 
 //Dates
+//Probably going to change to 1,5,10,all 
+dates = [];
 
-dates = ["all"];
+var oldestRecord = 1956; 
+var currentYear = new Date().getFullYear(); 
+
+for (i = oldestRecord; i <= currentYear; ++i) {
+  dates.unshift(i); 
+}
+
+dates.unshift("All"); 
+
+var dateSelect = document.getElementById('date');
+
+for (const date in dates) {
+  var newDateOption = document.createElement("option");
+  newDateOption.text = `${dates[date]}`;
+  dateSelect.add(newDateOption); 
+}
+
 
 
 /*
@@ -44,9 +74,34 @@ eventCSV = ["Men's 50 FR LCM", "Women's 50 FR LCM", "Men's 100 FR LCM",
             "Mixed 400 FR-R LCM", "Men's 800 FR-R LCM", "Women's 800 FR-R LCM", 
             "Men's 400 MED-R LCM", "Women's 400 MED-R LCM", "Mixed 400 MED-R LCM"]  
 
-/* Clean Up Data
-*/
 
+
+LCM_World_Records = []; 
+SCM_World_Records = []; 
+//Other World Records
+
+
+/*isRecord 
+ * Input: Record Object
+ *
+ * Only checks time to see if it is a 
+ * real swim time. 
+ * COULD ADD MORE ERROR CHECKING
+ *  
+ * Output: Boolean
+ *
+ */
+function isRecord(record) {
+  let timeRegexLong = new RegExp('^[0-9]+:+[0-9]+.+[0-9]');
+  let timeRegexShort = new RegExp('^[0-9]+.+[0-9]');
+  if(timeRegexLong.exec(record['Time']) || timeRegexShort.exec(record['Time'])) {
+    return true; 
+  } else {
+    return false; 
+  }
+}
+
+//Checks array for data 
 function includes(data, array) {
   for (i of array) {
     if(i == data) {
@@ -57,14 +112,92 @@ function includes(data, array) {
   return false; 
 }
 
+function converCountry(str) {
+  return str; 
+}
 
-function cleanUpData(data) {
+/*timeToMS 
+ * Input: time string (00:00.00 or 00.00)
+ * 
+ * Splits string and then converts minutes
+ * and seconds to milliseconds. 
+ * 
+ * Returns: Total time in milliseconds as 
+ * an integer. 
+ *
+ */
+function timeToMS(timeStr) {
+  var time = 0; 
+  //Regex matching for long vs short times
+  let timeRegexLong = new RegExp('^[0-9]+:+[0-9]+.+[0-9]');
+  let timeRegexShort = new RegExp('^[0-9]+.+[0-9]');
+  //Convert long times (00:00.00)
+  if(timeRegexLong.exec(timeStr)) {
+    var timeArray = timeStr.split(":"); 
+    timeArray.push(timeArray[1].split(".")[0]); 
+    timeArray.push(timeArray[1].split(".")[1]);
+    
+    
+    var minutes = Number(timeArray[0]) * 60000; 
+    var seconds = Number(timeArray[2]) * 1000;
+    var milliseconds = Number(timeArray[3]); 
+    
+    time = minutes + seconds + milliseconds; 
+
+  }
+  //Convert short times (00.00) 
+  else if (timeRegexShort.exec(timeStr)) {
+    var timeArray = timeStr.split(".");
+    
+    var seconds = Number(timeArray[0]) * 1000;
+    var milliseconds = Number(timeArray[1]); 
+    time = seconds + milliseconds; 
+
+  } else {
+    console.log("Error in time!"); 
+  }
+  
+  return time; 
+}
+
+
+/*cleanUp
+ * Input: Record Object
+ * 
+ * Takes a record object and converts:
+ *  Country Ab. => Full Country Name
+ *  Date String => JS DateTime
+ *  Rank String => Rank Int
+ *  Time String => Total Time Milliseconds Int
+ * 
+ * Output: 'Clean' Record
+ */
+function cleanUp(record) {
+  
+  record['Country'] = converCountry(record['Country']); 
+  record['Date'] = new Date(record['Date']);
+  record['Rank'] = Number(record['Rank']); 
+  record['Total Time'] = timeToMS(record['Time']); 
+  
+  return record;
+}
+
+
+/*fillArray
+ * Input: CSV Data for USA Swimming
+ * 
+ * Reads in event name and then adds to 
+ * each object. Calls cleanUp to convert
+ * text data to proper format. 
+ * 
+ * Adds record to array
+ */
+function fillArray(data) {
   
   var event = eventCSV[0]; 
  
-
   for (line in data) {
-
+    //Add event name to object 
     //Get Event
     const values = Object.values(data[line]); 
     var value1 = values[0]; 
@@ -73,19 +206,19 @@ function cleanUpData(data) {
     if(includes(value1, eventCSV)) {
       event = value1; 
     }
-
     //Add Event
-    data[line].Event = event; 
-    
+    data[line].Event = event;
 
-    console.log(data[line]); 
-
+    if(isRecord(data[line])) {
+      //Clean up object
+      let record = cleanUp(data[line]); 
+      console.log(record); 
+      //Add object to array
+      LCM_World_Records.push(record)
+    } 
   }
-
-
-
 }
 
 
-d3.csv("AllWorldRecords.csv").then( cleanUpData ); 
-
+//Fill LCM_World_Records
+d3.csv("AllWorldRecords.csv").then( fillArray ); 
